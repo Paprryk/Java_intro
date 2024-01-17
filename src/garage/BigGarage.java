a8bdae7e722c1d2fd11619b75fbb4f1dec9a9e1a
Papryk <patryk040600@gmail.com>
2024-01-17 16:04:30 +0000

    Further improvements to ArrayList

--- src/garage/BigGarage.java
+++ src/garage/BigGarage.java
@@ -3,13 +3,7 @@ package garage;
 import java.util.ArrayList;
 
 public class BigGarage {
-	private String name;
 	private ArrayList<Vehicle> vehicles = new ArrayList<>();
-
-	public BigGarage(String name) {
-		super();
-		this.name = name;
-	}
 
 //	add vehicle
 	public void addVehicle(Vehicle vehicle) {
@@ -21,15 +15,13 @@ public class BigGarage {
 		vehicles.remove(vehicle);
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
+// remove vehicle by index
+	public void removeVehicle(int vehicle) {
+		vehicles.remove(vehicle);
 	}
 
 	public void print() {
+		System.out.println("Richmans Garage");
 		for (Vehicle vehicle : vehicles) {
 			vehicle.print();
 		}
--- src/garage/ListGarage.java
+++ src/garage/ListGarage.java
@@ -2,10 +2,11 @@ package garage;
 
 public class ListGarage {
 	public static void main(String[] args) {
-		BigGarage myBigGarage = new BigGarage("Richmans Garage");
+		BigGarage myBigGarage = new BigGarage();
 		myBigGarage.addVehicle(new Car("Car", "BMW", "e45", "Silver", 5));
 		myBigGarage.addVehicle(new Bike("Bike", "Carrera", "ZX2", "White & Yellow", "Dirt"));
 		myBigGarage.addVehicle(new Plane("Plane", "Boeing", "737", "Blue & Yellow", 200));
+		myBigGarage.removeVehicle(1);
 
 		myBigGarage.print();
 	}
